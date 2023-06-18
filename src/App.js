48f15afca5d85ccd8b4be0dac1be394bc62f9bfc
Soumitra Saxena <soumitrasaxena@dreamworks.com>
2023-06-17 17:21:07 -0700

    [DEV] Added d3.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import * as d3 from "d3"
+import { useState, useEffect, useRef } from "react"
+
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <svg>
+      <circle 
+        cx="50"
+        cy="50"
+        r="50"
+      />
+    </svg>
+  )
 }
 
-export default App;
+export default App
